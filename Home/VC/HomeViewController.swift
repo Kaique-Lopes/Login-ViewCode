666efe073242f12d8c0a19521cf50e8e071b7c77
Kaique <kaique.lopes3271@gmail.com>
2022-01-03 15:47:22 -0300

    Create Protocols and custom cell

--- Home/VC/HomeViewController.swift
+++ Home/VC/HomeViewController.swift
@@ -8,13 +8,35 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-    var homescreen = HomeScreen()
+    var homescreen: HomeScreen?
+    
+    override func loadView() {
+        self.homescreen = HomeScreen()
+        self.view = homescreen
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.homescreen = HomeScreen()
-        self.view = self.homescreen
-
+        self.homescreen?.configTableViewProtocols(delegate: self, dataSource: self)
     }
 
+}
+
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let colors: [UIColor] = [.cyan, .orange, .blue, .gray]
+        let cell = UITableViewCell()
+        cell.backgroundColor = colors[indexPath.row]
+        return cell
+    }
+    
+    
+}
+
+extension HomeViewController: UITableViewDelegate {
+    
 }
