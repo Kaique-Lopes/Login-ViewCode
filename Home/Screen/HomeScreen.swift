c18daae5ca8eeda1f5c0170d1f95e86669aa85d6
Kaique <kaique.lopes3271@gmail.com>
2022-01-03 15:02:06 -0300

    Create tableView

--- Home/Screen/HomeScreen.swift
+++ Home/Screen/HomeScreen.swift
@@ -9,5 +9,28 @@ import UIKit
 
 class HomeScreen: UIView {
 
+    lazy var tableView: UITableView = {
+        var tableview = UITableView()
+        tableview.translatesAutoresizingMaskIntoConstraints = false
+        tableview.backgroundColor = .white
+         return tableview
+    }()
+    
+    override init(frame: CGRect){
+        super.init(frame: frame)
+        self.addSubview()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func addSubview() {
+        self.addSubview(tableView)
+    }
+    
+    func setupConstraints() {
+        
+    }
 
 }
