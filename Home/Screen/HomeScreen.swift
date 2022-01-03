95485cc1ca0c071b3af048cbbbbef73f111f546c
Kaique <kaique.lopes3271@gmail.com>
2022-01-03 15:46:28 -0300

    create config protocols tableview and constraints

--- Home/Screen/HomeScreen.swift
+++ Home/Screen/HomeScreen.swift
@@ -22,16 +22,28 @@ class HomeScreen: UIView {
         self.setupConstraints()
     }
     
+    func addSubview() {
+        self.addSubview(self.tableView)
+    }
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func addSubview() {
-        self.addSubview(tableView)
+    func configTableViewProtocols(delegate: UITableViewDelegate, dataSource: UITableViewDataSource) {
+        self.tableView.delegate = delegate
+        self.tableView.dataSource = dataSource
     }
     
+    
     func setupConstraints() {
-        self.tableView.anchor(top: self.safeAreaLayoutGuide.topAnchor, left: self.leftAnchor, right: self.rightAnchor)
+//        self.tableView.anchor(top: self.safeAreaLayoutGuide.topAnchor, left: self.leftAnchor, right: self.rightAnchor)
+        NSLayoutConstraint.activate([
+            self.tableView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor),
+            self.tableView.leftAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leftAnchor),
+            self.tableView.rightAnchor.constraint(equalTo: self.safeAreaLayoutGuide.rightAnchor),
+            self.tableView.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor)
+        ])
     }
 
 }
