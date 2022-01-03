1153e173ddbd21f647f6a016b756757a9e3513fa
Kaique <kaique.lopes3271@gmail.com>
2022-01-02 23:06:17 -0300

    func getAlert

--- Alert/Alert.swift
+++ Alert/Alert.swift
@@ -17,6 +17,12 @@ class Alert: NSObject {
     }
     
     func getAlert(title: String, message: String, completion:(() -> Void)? = nil ) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let cancell = UIAlertAction(title: "OK", style: .cancel) { action in
+            completion?()
+        }
+        alertController.addAction(cancell)
+        self.controller.present(alertController, animated: true, completion: nil)
         
     }
 }
