6b3fdbfc5adc1174993a66aadb7f6deec1d908c6
Kaique <kaique.lopes3271@gmail.com>
2022-01-03 22:17:13 -0300

    create init and setupConstraints

--- Home/Cell/UserDetailTableViewCell.swift
+++ Home/Cell/UserDetailTableViewCell.swift
@@ -29,10 +29,36 @@ class UserDetailTableViewCell: UITableViewCell {
         return label
     }()
     
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?){
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        addSubview()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     // MARK: - Create Function for add elements in view
     func addSubview() {
         self.contentView.addSubview(userImageView)
         self.contentView.addSubview(userLabel)
 
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            
+            // MARK: - userImageView - UIImageview
+            self.userImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            self.userImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            self.userImageView.heightAnchor.constraint(equalToConstant: 80),
+            self.userImageView.widthAnchor.constraint(equalToConstant: 80),
+        
+            // MARK: - userLabel - Label
+            self.userLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            self.userLabel.leadingAnchor.constraint(equalTo: self.userImageView.trailingAnchor, constant: 25),
+
+        ])
     }
 }
