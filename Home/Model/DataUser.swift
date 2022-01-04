ec42556d21f64ba55257dac05b198eba9e3c0b34
Kaique <kaique.lopes3271@gmail.com>
2022-01-03 22:42:53 -0300

    Create DataUser

---
+++ Home/Model/DataUser.swift
@@ -0,0 +1,13 @@
+//
+//  DataUser.swift
+//  Login-ViewCode
+//
+//  Created by Kaique Lopes de Oliveira on 03/01/22.
+//
+
+import Foundation
+
+struct DataUser {
+    var name: String
+    var imageName: String
+}
