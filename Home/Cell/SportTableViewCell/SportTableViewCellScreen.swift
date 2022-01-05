c2194255a54ab0d82c963c7fb6c7ae4a4bd90b5c
Kaique <kaique.lopes3271@gmail.com>
2022-01-05 00:01:08 -0300

    SportTableViewCellScreen

---
+++ Home/Cell/SportTableViewCell/SportTableViewCellScreen.swift
@@ -0,0 +1,20 @@
+//
+//  SportTableViewCellScreen.swift
+//  Login-ViewCode
+//
+//  Created by Kaique Lopes de Oliveira on 05/01/22.
+//
+
+import UIKit
+
+class SportTableViewCellScreen: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
