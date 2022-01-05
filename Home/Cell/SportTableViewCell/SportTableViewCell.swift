6123a0f1add95629b955013727e5d23475b41e03
Kaique <kaique.lopes3271@gmail.com>
2022-01-04 23:56:59 -0300

    Create SportTableViewCell.swift

---
+++ Home/Cell/SportTableViewCell/SportTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SportTableViewCell.swift
+//  Login-ViewCode
+//
+//  Created by Kaique Lopes de Oliveira on 04/01/22.
+//
+
+import UIKit
+
+class SportTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
