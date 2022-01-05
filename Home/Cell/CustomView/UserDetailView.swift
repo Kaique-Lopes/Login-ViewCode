59b7f18df21ec65fac85fbb3f345219c59f6c9d3
Kaique <kaique.lopes3271@gmail.com>
2022-01-04 22:58:09 -0300

    Create UserDetailVIew

---
+++ Home/Cell/CustomView/UserDetailView.swift
@@ -0,0 +1,16 @@
+//
+//  UserDetailView.swift
+//  Login-ViewCode
+//
+//  Created by Kaique Lopes de Oliveira on 04/01/22.
+//
+
+import UIKit
+
+class UserDetailView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+}
