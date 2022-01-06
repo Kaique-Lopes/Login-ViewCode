027c89af63381021baa4628a9aa7d8ed59ff0438
Kaique <kaique.lopes3271@gmail.com>
2022-01-05 23:13:15 -0300

    Update SportTableViewCellScreen.swift

--- Home/Cell/SportTableViewCell/SportTableViewCellScreen.swift
+++ Home/Cell/SportTableViewCell/SportTableViewCellScreen.swift
@@ -19,4 +19,12 @@ class SportTableViewCellScreen: UIView {
         collectionView.setCollectionViewLayout(layout, animated: false)
         return collectionView
     }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
