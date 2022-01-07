bbe357c1fecb8c0fe0442d0c20ec6f63127b29c6
Kaique <kaique.lopes3271@gmail.com>
2022-01-06 23:54:42 -0300

    Update SportTableViewCell.swift

--- Home/Cell/SportTableViewCell/SportTableViewCell.swift
+++ Home/Cell/SportTableViewCell/SportTableViewCell.swift
@@ -14,9 +14,20 @@ class SportTableViewCell: UITableViewCell {
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.addSubviews()
+        self.configConstraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func addSubviews() {
+        self.sportTableViewCellScreen.translatesAutoresizingMaskIntoConstraints = false
+        self.contentView.addSubview(self.sportTableViewCellScreen)
+    }
+    
+    func configConstraints() {
+            self.sportTableViewCellScreen.anchor(top: self.topAnchor, left: self.leftAnchor, bottom: self.bottomAnchor, right: self.rightAnchor)
     }
 }
