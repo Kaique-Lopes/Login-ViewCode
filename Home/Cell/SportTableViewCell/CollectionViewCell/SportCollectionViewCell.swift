6ba5b8f696ad08e71118e5d1bfbc972f7cfd5bc1
Kaique <kaique.lopes3271@gmail.com>
2022-01-11 22:29:38 -0300

    Create SportCollectionViewCell

---
+++ Home/Cell/SportTableViewCell/CollectionViewCell/SportCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  SportCollectionViewCell.swift
+//  Login-ViewCode
+//
+//  Created by Kaique Lopes de Oliveira on 11/01/22.
+//
+
+import UIKit
+
+class SportCollectionViewCell: UICollectionViewCell {
+    
+}
