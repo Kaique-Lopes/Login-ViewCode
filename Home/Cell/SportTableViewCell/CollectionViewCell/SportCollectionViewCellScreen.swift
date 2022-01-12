2a77ecfa1b724272a068e1cbb3dc6c8ab4c25e0c
Kaique <kaique.lopes3271@gmail.com>
2022-01-11 22:43:51 -0300

    Update SportCollectionViewCellScreen.swift

--- Home/Cell/SportTableViewCell/CollectionViewCell/SportCollectionViewCellScreen.swift
+++ Home/Cell/SportTableViewCell/CollectionViewCell/SportCollectionViewCellScreen.swift
@@ -9,17 +9,29 @@ import UIKit
 
 class SportCollectionViewCellScreen: UIView {
 
+    // MARK: - Criação do componente viewBackground
     lazy var viewBackground: UIView = {
         var view = UIView()
         view.backgroundColor = .cyan
         return view
     }()
 
+    // MARK: - Inicializador
     override init(frame: CGRect = .zero) {
         super.init(frame: frame)
+        self.addSubview()
     }
     
+    // MARK: - Erro
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    // MARK: - Adicionando Elemento na view
+    func addSubview(){
+        self.addSubview(viewBackground)
+    }
+    
+    // MARK: - Configuração das Constraints dos componentes
+    func setupConstraints() {}
 }
