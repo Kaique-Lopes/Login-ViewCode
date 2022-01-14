2dc3c4d1b7ddef08230e7dcae37dee34625dbe20
Kaique <kaique.lopes3271@gmail.com>
2022-01-13 23:04:21 -0300

    model Sport

---
+++ Home/Model/Sport.swift
@@ -0,0 +1,14 @@
+//
+//  Sport.swift
+//  Login-ViewCode
+//
+//  Created by Kaique Lopes de Oliveira on 13/01/22.
+//
+
+import Foundation
+import UIKit
+
+struct Sport {
+    var name: String
+    var image: String
+}
